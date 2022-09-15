11730ae3ce409eb1cf6c748edd6937117bfe2ce6
Egor Ternovoy <cofob@riseup.net>
2022-09-15 17:00:42 +0700

    Add blueline to error page

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,19 +1,19 @@
 <script lang="ts">
 	import { Meta, Section } from "$lib/components";
 	import { page } from "$app/stores";
+	import BlueLine from "$src/lib/components/BlueLine.svelte";
 </script>
 
 <Meta title={$page.status.toString()} description="Ошибка {$page.status}" noindex />
 
 <Section>
 	<div class="text-center">
-		<h1 class="sm:text-2xl text-xl font-semibold">
-			{#if $page.status == 404}
-				Страница не найдена
-			{:else}
-				Ошибка {$page.status}
-			{/if}
+		<h1 class="sm:text-7xl text-5xl font-semibold">
+			<BlueLine animate>{$page.status}.</BlueLine>
 		</h1>
+		{#if $page.status == 404}
+			<p>Страница не найдена</p>
+		{/if}
 		<p>{$page.error?.message}</p>
 		<a href="/" class="link">На главную -></a>
 	</div>
