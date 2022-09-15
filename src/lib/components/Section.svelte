9e1c2144cf35086ff40f9f379a835d0746dde494
Egor Ternovoy <cofob@riseup.net>
2022-09-15 16:49:38 +0700

    Add Section component

---
+++ src/lib/components/Section.svelte
@@ -0,0 +1,1 @@
+<section class="max-w-screen-lg my-10"><slot /></section>
--- src/lib/components/index.ts
+++ src/lib/components/index.ts
@@ -2,3 +2,4 @@ export { default as Meta } from "./Meta.svelte";
 export { default as Footer } from "./Footer.svelte";
 export { default as Navbar } from "./Navbar.svelte";
 export { default as BlueLine } from "./BlueLine.svelte";
+export { default as Section } from "./Section.svelte";
