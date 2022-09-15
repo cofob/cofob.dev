93e6d07bafa6cc501008d55b65853237bb2effd1
Egor Ternovoy <cofob@riseup.net>
2022-09-15 16:54:22 +0700

    Update index page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,18 @@
 <script lang="ts">
 	import { Meta, BlueLine } from "$lib/components";
+import Section from "$src/lib/components/Section.svelte";
 </script>
 
 <Meta title="Главная" description="Персональный сайт кофоба" />
 
-<div class="max-w-screen-lg mx-auto my-10">
-	<p class="text-center text-7xl font-medium">
-		Привет! Я <BlueLine animate>cofob</BlueLine>.
-	</p>
-</div>
+<Section>
+	<div class="text-center">
+		<p class="text-7xl font-medium">
+			Привет! Я <BlueLine animate>cofob</BlueLine>.
+		</p>
+		<p>Разработчик Python, Rust, Web приложений.</p>
+		<a href="/portfolio">
+			<button class="mt-2 font-semibold text-md rounded-lg border-2 border-zinc-800 p-1"> Портфолио -> </button>
+		</a>
+	</div>
+</Section>
