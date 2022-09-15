c578e4e4f6f6cb89aa07acb9d178197463d18246
Egor Ternovoy <cofob@riseup.net>
2022-09-15 16:03:21 +0700

    Adaptive blue line

--- src/lib/components/Cofob.svelte
+++ src/lib/components/Cofob.svelte
@@ -4,8 +4,8 @@
 
 <style>
 	.top {
-		@apply before:block before:absolute before:-inset-1 before:-skew-y-6 before:h-3
-					 before:mt-3 before:ml-4 before:w-8 before:bg-sky-300 before:rounded-full
+		@apply before:block before:absolute before:h-2/4 before:w-3/4 before:bottom-0.5
+					 before:-right-1 before:-skew-y-6 before:rounded-full before:bg-sky-300
 					 relative inline-block;
 	}
 
