789dac03a1008971ef821ee2bb9fc54a0b193ca7
Egor Ternovoy <cofob@riseup.net>
2022-09-15 16:54:10 +0700

    Error page

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,14 +1,20 @@
 <script lang="ts">
-	import { Meta } from "$lib/components";
+	import { Meta, Section } from "$lib/components";
 	import { page } from "$app/stores";
 </script>
 
 <Meta title={$page.status.toString()} description="Ошибка {$page.status}" noindex />
 
-<div>
-	{#if $page.status == 404}
-		<p>Страница не найдена</p>
-	{/if}
-	<p>Ошибка: {$page.error?.message}</p>
-	<a href="/" class="link">На главную -></a>
-</div>
+<Section>
+	<div class="text-center">
+		<h1 class="sm:text-2xl text-xl font-semibold">
+			{#if $page.status == 404}
+				Страница не найдена
+			{:else}
+				Ошибка {$page.status}
+			{/if}
+		</h1>
+		<p>{$page.error?.message}</p>
+		<a href="/" class="link">На главную -></a>
+	</div>
+</Section>
