101994ef3e54168386ef9297cdbc19155b44f751
Egor Ternovoy <cofob@riseup.net>
2022-09-15 16:45:03 +0700

    Add source code link to footer

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -2,6 +2,13 @@
 	import BlueLine from "./BlueLine.svelte";
 </script>
 
-<footer>
-	<p class="text-center my-4">(c) <BlueLine>cofob</BlueLine> 2021--2022</p>
+<footer class="text-center my-4">
+	<p>(c) <BlueLine>cofob</BlueLine> 2021--2022</p>
+	<p><a href="https://git.frsqr.xyz/cofob/cofob.ru">Исходный код сайта</a></p>
 </footer>
+
+<style>
+	a {
+		@apply hover:underline;
+	}
+</style>
