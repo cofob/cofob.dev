9a80d88048deaf6dbc98abe1f64c4b3dbb86cfcc
Egor Ternovoy <cofob@riseup.net>
2023-11-16 11:53:52 +0700

    2022 -> 2023

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -3,7 +3,7 @@
 </script>
 
 <footer class="text-center my-4">
-	<p>(c) <BlueLine>cofob</BlueLine> 2021--2022</p>
+	<p>(c) <BlueLine>cofob</BlueLine> 2021--2023</p>
 	<p><a href="https://github.com/cofob/cofob.dev">Исходный код сайта</a></p>
 </footer>
 
--- src/routes/sitemap.xml/+server.ts
+++ src/routes/sitemap.xml/+server.ts
@@ -18,13 +18,13 @@ export async function GET() {
 				<loc>/</loc>
 				<changefreq>weekly</changefreq>
 				<priority>1</priority>
-				<lastmod>2022-09-15</lastmod>
+				<lastmod>2023-11-16</lastmod>
 			</url>
 			<url>
 				<loc>/portfolio/</loc>
 				<changefreq>weekly</changefreq>
 				<priority>0.7</priority>
-				<lastmod>2022-09-15</lastmod>
+				<lastmod>2023-11-16</lastmod>
 			</url>
 		</urlset>`,
 		{ headers: headers },
