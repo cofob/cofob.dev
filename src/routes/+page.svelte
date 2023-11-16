d5434c61c43f48ffc23de8520a7e6a1deed66445
Egor Ternovoy <cofob@riseup.net>
2023-11-16 11:55:42 +0700

    Update index

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,7 +9,7 @@
 		<Heading level={1}>
 			Привет! Я <BlueLine animate>cofob</BlueLine>.
 		</Heading>
-		<p>Разработчик Python, Rust, Web приложений.</p>
+		<p>Разработчик Python, Web приложений.</p>
 		<a href="/portfolio">
 			<button class="mt-2 font-semibold text-md rounded-lg border-2 border-zinc-800 p-1"> Портфолио -> </button>
 		</a>
