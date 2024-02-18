54988d53e33ba82d5eb4a6bf95853638024dc9ab
cofob <cofob@riseup.net>
2024-02-18 21:19:46 +0700

    Update hash

--- package.nix
+++ package.nix
@@ -8,7 +8,7 @@ buildNpmPackage {
 
   nodejs = nodejs;
 
-  npmDepsHash = "sha256-aKeZzYTka32uUk1umewl2FBjD9TtcvjaybfFepvWYoE=";
+  npmDepsHash = "sha256-LVCMhQ9h47H6u7yr5ihfqw2FoD2oFKclp1ooelq9SSU=";
 
   installPhase = ''
     cp -r build $out
