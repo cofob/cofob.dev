60cda26cbd576c1fdda75eac0be0330b8b5cabd2
cofob <cofob@riseup.net>
2024-03-07 16:24:22 +0400

    Compress static assets

--- package.nix
+++ package.nix
@@ -1,4 +1,4 @@
-{ buildNpmPackage, nodejs, bash }:
+{ buildNpmPackage, nodejs, bash, gzip, brotli }:
 
 buildNpmPackage {
   pname = "cofob-dev";
@@ -10,6 +10,12 @@ buildNpmPackage {
 
   npmDepsHash = "sha256-LVCMhQ9h47H6u7yr5ihfqw2FoD2oFKclp1ooelq9SSU=";
 
+  postBuild = ''
+    # compress static
+    find build/client/static -type f -print0 | xargs -0 -I{} \
+      sh -c "${gzip}/bin/gzip -c --best {} > {}.gz && ${brotli}/bin/brotli -c --best {} > {}.br"
+  '';
+
   installPhase = ''
     cp -r build $out
     cp -r node_modules $out/
@@ -17,8 +23,7 @@ buildNpmPackage {
 
     mkdir $out/bin
     cat <<EOF > $out/bin/cofob-dev
-      #!${bash}/bin/bash
-      ${nodejs}/bin/node $out/index.js
+      #!${nodejs}/bin/node $out/index.js
     EOF
     chmod u+x $out/bin/cofob-dev
   '';
